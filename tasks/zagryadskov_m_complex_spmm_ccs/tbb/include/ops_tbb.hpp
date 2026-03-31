2b99ff1e2c3c78240d7a51902ff951ebda025ce4
zagriadskov maksim <maksimzagradskrv50910@gmail.com>
2026-03-31 20:47:55 +0300

    First realization

--- tasks/zagryadskov_m_complex_spmm_ccs/tbb/include/ops_tbb.hpp
+++ tasks/zagryadskov_m_complex_spmm_ccs/tbb/include/ops_tbb.hpp
@@ -1,9 +1,25 @@
 #pragma once
 
-#include "zagryadskov_m_complex_spmm_ccs/common/include/common.hpp"
+#include <tbb/enumerable_thread_specific.h>
+
+#include <complex>
+#include <vector>
+
 #include "task/include/task.hpp"
+#include "zagryadskov_m_complex_spmm_ccs/common/include/common.hpp"
 
 namespace zagryadskov_m_complex_spmm_ccs {
+
+struct LocalData {
+  std::vector<int> row_ind;
+  std::vector<std::complex<double>> values;
+  std::vector<int> col_ptr;
+
+  std::vector<int> marker;
+  std::vector<std::complex<double>> acc;
+
+  LocalData(int m, std::complex<double> zero);
+};
 
 class ZagryadskovMComplexSpMMCCSTBB : public BaseTask {
  public:
@@ -13,6 +29,9 @@ class ZagryadskovMComplexSpMMCCSTBB : public BaseTask {
   explicit ZagryadskovMComplexSpMMCCSTBB(const InType &in);
 
  private:
+  static void SpMM(const CCS &a, const CCS &b, CCS &c);
+  static void SpMMkernel(const CCS &a, const CCS &b, const std::complex<double> &zero, double eps,
+                         tbb::enumerable_thread_specific<LocalData> &tls, std::vector<int> &col_counts);
   bool ValidationImpl() override;
   bool PreProcessingImpl() override;
   bool RunImpl() override;
--- tasks/zagryadskov_m_complex_spmm_ccs/tbb/src/ops_tbb.cpp
+++ tasks/zagryadskov_m_complex_spmm_ccs/tbb/src/ops_tbb.cpp
@@ -1,14 +1,14 @@
 #include "zagryadskov_m_complex_spmm_ccs/tbb/include/ops_tbb.hpp"
 
+#include <tbb/blocked_range.h>
+#include <tbb/enumerable_thread_specific.h>
 #include <tbb/tbb.h>
 
-#include <atomic>
-#include <numeric>
 #include <util/include/util.hpp>
 #include <vector>
 
-#include "zagryadskov_m_complex_spmm_ccs/common/include/common.hpp"
 #include "oneapi/tbb/parallel_for.h"
+#include "zagryadskov_m_complex_spmm_ccs/common/include/common.hpp"
 
 namespace zagryadskov_m_complex_spmm_ccs {
 
@@ -16,6 +16,87 @@ ZagryadskovMComplexSpMMCCSTBB::ZagryadskovMComplexSpMMCCSTBB(const InType &in) {
   SetTypeOfTask(GetStaticTypeOfTask());
   GetInput() = in;
   GetOutput() = CCS();
+}
+
+LocalData::LocalData(int m, std::complex<double> zero) : marker(m, -1), acc(m, zero) {}
+
+void ZagryadskovMComplexSpMMCCSTBB::SpMMkernel(const CCS &a, const CCS &b, const std::complex<double> &zero, double eps,
+                                               tbb::enumerable_thread_specific<LocalData> &tls,
+                                               std::vector<int> &col_counts) {
+  tbb::parallel_for(tbb::blocked_range<int>(0, b.n), [&](const tbb::blocked_range<int> &r) {
+    auto &local = tls.local();
+
+    std::vector<int> rows;
+
+    for (int j = r.begin(); j < r.end(); ++j) {
+      rows.clear();
+
+      for (int k = b.col_ptr[j]; k < b.col_ptr[j + 1]; ++k) {
+        std::complex<double> tmpval = b.values[k];
+        int btmpind = b.row_ind[k];
+
+        for (int zp = a.col_ptr[btmpind]; zp < a.col_ptr[btmpind + 1]; ++zp) {
+          int atmpind = a.row_ind[zp];
+          local.acc[atmpind] += tmpval * a.values[zp];
+
+          if (local.marker[atmpind] != j) {
+            rows.push_back(atmpind);
+            local.marker[atmpind] = j;
+          }
+        }
+      }
+
+      int count = 0;
+
+      for (int tmpind : rows) {
+        if (std::abs(local.acc[tmpind]) > eps) {
+          local.values.push_back(local.acc[tmpind]);
+          local.row_ind.push_back(tmpind);
+          ++count;
+        }
+        local.acc[tmpind] = zero;
+      }
+
+      col_counts[j] = count;
+    }
+  });
+}
+
+void ZagryadskovMComplexSpMMCCSTBB::SpMM(const CCS &a, const CCS &b, CCS &c) {
+  c.m = a.m;
+  c.n = b.n;
+  c.col_ptr.assign(b.n + 1, 0);
+  c.row_ind.clear();
+  c.values.clear();
+  std::complex<double> zero(0.0, 0.0);
+  const double eps = 1e-14;
+  std::vector<int> col_counts(b.n, 0);
+
+  tbb::enumerable_thread_specific<LocalData> tls([&]() { return LocalData(a.m, zero); });
+
+  SpMMkernel(a, b, zero, eps, tls, col_counts);
+
+  for (int j = 0; j < b.n; ++j) {
+    c.col_ptr[j + 1] = c.col_ptr[j] + col_counts[j];
+  }
+
+  c.row_ind.resize(c.col_ptr[b.n]);
+  c.values.resize(c.col_ptr[b.n]);
+
+  std::vector<int> offsets = c.col_ptr;
+
+  for (auto &local : tls) {
+    int ptr = 0;
+    for (int j = 0; j < b.n; ++j) {
+      int cnt = col_counts[j];
+      for (int k = 0; k < cnt; ++k) {
+        int pos = offsets[j]++;
+        c.row_ind[pos] = local.row_ind[ptr];
+        c.values[pos] = local.values[ptr];
+        ++ptr;
+      }
+    }
+  }
 }
 
 bool ZagryadskovMComplexSpMMCCSTBB::ValidationImpl() {
@@ -29,24 +110,12 @@ bool ZagryadskovMComplexSpMMCCSTBB::PreProcessingImpl() {
 }
 
 bool ZagryadskovMComplexSpMMCCSTBB::RunImpl() {
-  // for (InType i = 0; i < GetInput(); i++) {
-  //   for (InType j = 0; j < GetInput(); j++) {
-  //     for (InType k = 0; k < GetInput(); k++) {
-  //       std::vector<InType> tmp(i + j + k, 1);
-  //       GetOutput() += std::accumulate(tmp.begin(), tmp.end(), 0);
-  //       GetOutput() -= i + j + k;
-  //     }
-  //   }
-  // }
+  const CCS &a = std::get<0>(GetInput());
+  const CCS &b = std::get<1>(GetInput());
+  CCS &c = GetOutput();
 
-  // const int num_threads = ppc::util::GetNumThreads();
-  // GetOutput() *= num_threads;
+  ZagryadskovMComplexSpMMCCSTBB::SpMM(a, b, c);
 
-  // std::atomic<int> counter(0);
-  // tbb::parallel_for(0, ppc::util::GetNumThreads(), [&](int /*i*/) { counter++; });
-
-  // GetOutput() /= counter;
-  // return GetOutput() > 0;
   return true;
 }
 
