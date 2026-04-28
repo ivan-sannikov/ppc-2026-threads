36ad8ab213672c206bdeeb7b9b7239fdb616a488
MaRiVaNa129 <132110214+MaRiVaNa129@users.noreply.github.com>
2026-04-29 02:02:18 +0300

    fix chec

--- tasks/shkenev_i_constra_hull_for_binary_image/tbb/src/ops_tbb.cpp
+++ tasks/shkenev_i_constra_hull_for_binary_image/tbb/src/ops_tbb.cpp
@@ -18,6 +18,7 @@ namespace shkenev_i_constra_hull_for_binary_image {
 
 namespace {
 
+// 128
 constexpr uint8_t kThreshold = 128;
 
 constexpr std::array<std::pair<int, int>, 4> kDirs = {{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}};
